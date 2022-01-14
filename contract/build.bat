402637db4e7a65ae4fd8117614966102ac770b73
allen0x1 <shdwopz@gmail.com>
2022-01-13 17:45:20 -0800

    Fixed wasm file name of the build target.

--- contract/build.bat
+++ contract/build.bat
@@ -1,2 +1,2 @@
 cargo build --target wasm32-unknown-unknown --release
-copy target\wasm32-unknown-unknown\release\rust_template.wasm res
+copy target\wasm32-unknown-unknown\release\crossword_tutorial_chapter_1.wasm res
--- contract/build.sh
+++ contract/build.sh
@@ -2,5 +2,5 @@
 set -e
 
 RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
-cp target/wasm32-unknown-unknown/release/rust_template.wasm ./res/
+cp target/wasm32-unknown-unknown/release/crossword_tutorial_chapter_1.wasm ./res/
 

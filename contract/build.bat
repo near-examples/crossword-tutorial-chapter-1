a1d1cf119e63b3a8005636b6db902dfa33ffa488
Austin <austinbaggio@gmail.com>
2024-01-19 07:37:43 -0500

    Replace rust_template with wildcard in mac + pc build files (#3)
    * Generalize build.sh for the Zero-to-Hero tutorial
    
    * Update build.bat

--- contract/build.bat
+++ contract/build.bat
@@ -1,2 +1,2 @@
 cargo build --target wasm32-unknown-unknown --release
-copy target\wasm32-unknown-unknown\release\rust_template.wasm res
+copy target\wasm32-unknown-unknown\release\*.wasm res 
--- contract/build.sh
+++ contract/build.sh
@@ -2,5 +2,4 @@
 set -e
 
 RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
-cp target/wasm32-unknown-unknown/release/rust_template.wasm ./res/
-
+cp target/wasm32-unknown-unknown/release/*.wasm ./res/
